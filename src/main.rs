70cf5572fff050f4e3e63d104269a27916fdb81e
amm <adrian.moya.martin@gmail.com>
2024-04-25 21:13:45 +0200

    rewrote it all

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
-use actix_web::*;
-use serde::Deserialize;
+use std::{
+     io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}
+};
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    println!("connection made");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => {
+                let stream = stream.unwrap();
+                handle_connection(stream)
+            },
+            Err(_) => {
+                println!(" error during connection ");
+            }
+        }
+    }
 }
+fn handle_connection(mut stream: TcpStream) {
+    
+    let mut buffer = [0; 2048];
+    let stream_data_size = stream.read(&mut buffer).unwrap();
 
-#[actix_web::get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("body")
-}
+    let buffer = vec![0; stream_data_size];
 
-#[actix_web::post("/")]
-async fn echo(req_body: web::Json<Info>) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json(format!("username: {}", req_body.username)))
-}
+    let mut headers = [httparse::EMPTY_HEADER; 4];
+    let mut req = httparse::Request::new(&mut headers);
+    println!("{buffer:?}");
+    let res = req.parse(&buffer).unwrap();
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| {
-        let _json_config = web::JsonConfig::default()
-            .limit(4096)
-            .error_handler(|err, _req| {
-                // create custom error response
-                error::InternalError::from_response(err, HttpResponse::Conflict().finish())
-                    .into()
-            });
-        App::new()
-            .service(hello)
-            .service(echo)
-    })
-    .workers(4)
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let buf_reader = BufReader::new(&mut stream);
+    
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Headers: {:?}", res);
 }
